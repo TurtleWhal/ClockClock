0562ecddc0d41b5efb15c9798675b6aabdefc2dd
TurtleWhal <garrettrjordan@gmail.com>
2025-02-24 10:16:33 -0800

    Added schematic and pins.h

---
+++ Code/src/pins.h
@@ -0,0 +1,56 @@
+// Pinout from Table 2-4 in Datasheet
+
+// ---- Motors ----
+
+// Motor 1
+#define M1_A1 10 // Pin 15
+#define M1_A2 9  // Pin 14
+#define M1_A3 8  // Pin 13
+#define M1_A4 11 // Pin 16
+#define M1_B1 4  // Pin 9
+#define M1_B2 6  // Pin 11
+#define M1_B3 7  // Pin 12
+#define M1_B4 5  // Pin 10
+
+// Motor 2
+#define M2_A1 14 // Pin 19
+#define M2_A2 13 // Pin 18
+#define M2_A3 12 // Pin 17
+#define M2_A4 15 // Pin 20
+#define M2_B1 45 // Pin 51
+#define M2_B2 1  // Pin 6
+#define M2_B3 2  // Pin 7
+#define M2_B4 46 // Pin 52
+
+// Motor 3
+#define M3_A1 16 // Pin 22
+#define M3_A2 21 // Pin 27
+#define M3_A3 18 // Pin 24
+#define M3_A4 17 // Pin 23
+#define M3_B1 41 // Pin 47
+#define M3_B2 39 // Pin 44
+#define M3_B3 40 // Pin 45
+#define M3_B4 42 // Pin 48
+
+// Motor 4
+#define M4_A1 47 // Pin 37
+#define M4_A2 34 // Pin 39
+#define M4_A3 33 // Pin 38
+#define M4_A4 48 // Pin 36
+#define M4_B1 37 // Pin 42
+#define M4_B2 35 // Pin 40
+#define M4_B3 36 // Pin 41
+#define M4_B4 38 // Pin 43
+
+// ---- Headers ----
+
+#define HEADER_1 3  // Header 1 (J10, GPIO3, Pin 8)
+#define HEADER_2 26 // Header 2 (J11, CS1, Pin 28)
+#define HEADER_3 0  // Header 3 (J13, BOOT, Pin 5)
+
+// ---- Buttons ----
+#define BUTTON_BOOT 0 // Boot Button (Pin 5)
+
+// ---- UART ----
+#define UART_A 43 // Pin 49
+#define UART_B 44 // Pin 50
